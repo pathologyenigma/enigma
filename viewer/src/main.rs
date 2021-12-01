4f5133cb490df607bf784d89fbba312cd18a90e7
pengyihao <pathology666@163.com>
2021-12-01 15:36:05 +0800

    finish window and launcher

--- viewer/src/main.rs
+++ viewer/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use enigma::app::{Application, Result, Runnable, Runner};
+struct Viewer;
+
+impl Runnable for Viewer {}
+
+fn main() -> Result<()> {
+    Application::build().run(Viewer {})
 }
